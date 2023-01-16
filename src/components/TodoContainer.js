90c7f2281421f11eb8e499b13058c38ea1ea5a50
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-17 01:42:11 +0600

    Update TodoContainer.js to import todoList.

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodosList from './TodosList';
 class TodoContainer extends React.Component {
   state = {
     todos: [
@@ -21,13 +22,7 @@ class TodoContainer extends React.Component {
   };
 
   render() {
-    return (
-      <ul>
-        {this.state.todos.map((todo) => (
-          <li>{todo.title}</li>
-        ))}
-      </ul>
-    );
+    return <TodosList todos={this.state.todos} />;
   }
 }
 export default TodoContainer;

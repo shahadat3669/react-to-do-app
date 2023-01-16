8a47d00f2b9d24599380d1d7796be5938fa7a4f6
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-17 04:16:33 +0600

    Validating the Form Submission

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 
 class InputTodo extends Component {
   state = {
-    fName: '',
-    lastName: '',
+    title: '',
   };
-
   onChange = (e) => {
     this.setState({
       [e.target.name]: e.target.value,
@@ -14,22 +12,28 @@ class InputTodo extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    this.props.addTodoProps(this.state.title);
-    this.setState({
-      title: '',
-    });
+    if (this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title);
+      this.setState({
+        title: '',
+      });
+    } else {
+      alert('Please write item');
+    }
   };
+
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit} className="form-container">
         <input
           type="text"
+          className="input-text"
           placeholder="Add todo..."
           value={this.state.title}
           name="title"
           onChange={this.onChange}
         />
-        <button>Submit</button>
+        <button className="input-submit">Submit</button>
       </form>
     );
   }

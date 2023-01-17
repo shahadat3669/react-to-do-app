830a32a2009f854c66c0de68178693ad0b36ecb0
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-17 22:57:00 +0600

    Fix linter error in TodoItem class.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,23 +1,47 @@
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
+
 import styles from './TodoItem.module.css';
 
 class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
   render() {
+    const { todo, handleChangeProps, deleteTodoProps } = this.props;
+    const { id, title, completed } = todo;
+
     return (
       <li className={styles.item}>
         <input
           type="checkbox"
           className={styles.checkbox}
-          checked={this.props.todo.completed}
-          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+          checked={completed}
+          onChange={() => handleChangeProps(id)}
         />
-        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+        <button type="button" onClick={() => deleteTodoProps(id)}>
           Delete
         </button>
-        {this.props.todo.title}
+        {title}
       </li>
     );
   }
 }
+
+TodoItem.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  completed: PropTypes.bool.isRequired,
+};
+
+TodoItem.propTypes = {
+  todo: TodoItem.isRequired,
+  handleChangeProps: PropTypes.func.isRequired,
+  deleteTodoProps: PropTypes.func.isRequired,
+};
 
 export default TodoItem;
